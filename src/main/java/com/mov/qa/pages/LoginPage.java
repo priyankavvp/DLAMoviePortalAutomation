d85e4962abe8c808681b02fcdad6d02bb2505228
priyankavvp <bandampriyanka13@gmail.com>
2022-11-24 21:31:36 +0530

    initial commit

---
+++ src/main/java/com/mov/qa/pages/LoginPage.java
@@ -0,0 +1,151 @@
+package com.mov.qa.pages;
+
+import java.util.List;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+
+import com.mov.qa.base.TestBase;
+
+public class LoginPage extends TestBase{
+	
+	//Page Factory - OR:
+	@FindBy(name="email")
+	WebElement username;
+	
+	@FindBy(name="password")
+	WebElement password;
+	
+	@FindBy(xpath="//input[@type='submit']")
+	WebElement loginBtn;
+	
+	@FindBy(xpath="//button[contains(text(),'Sign Up')]")
+	WebElement signUpBtn;
+	
+	@FindBy(xpath="//span[contains(text(),'Sign in with IMDb')]")
+	WebElement signinIMDB;
+	
+	@FindBy(id="suggestion-search")
+	WebElement searchBox;
+	
+	@FindBy(xpath="//div[contains(text(),'Pushpa: The Rise - Part 1')]")
+	WebElement pushpaMovielistItem;
+	
+	
+	@FindBy(xpath="//*[@id=\"react-autowhatever-1--item-0\"]")
+	WebElement MovielistItem;
+	
+	@FindBy(xpath="//*[@id=\"__next\"]/main/div/section[1]/div/section/div/div[1]/section[10]/div[2]/ul/li")
+	List<WebElement> movieDetailsList;
+	
+	@FindBy(name="search")
+	WebElement searchBoxWiki;
+	
+	
+	@FindBy(xpath="//*[@class=\"infobox vevent\"]/tbody/tr")
+	List<WebElement> movieWikiDetailsList;
+	
+	//Initializing the Page Objects:
+	public LoginPage(){
+		PageFactory.initElements(driver, this);
+	}
+	
+	//Actions:
+	public String validateLoginPageTitle(){
+		return driver.getTitle();
+	}
+	
+	public void  navigateToSigneIn(){
+		signinIMDB.click();
+	}
+	
+	public HomePage login(String un, String pwd){
+		username.sendKeys(un);
+		password.sendKeys(pwd);
+		//loginBtn.click();
+		    	JavascriptExecutor js = (JavascriptExecutor)driver;
+		    	js.executeScript("arguments[0].click();", loginBtn);
+		    	
+		return new HomePage();
+		
+	}
+	
+	public void searchMovie(String movieName)
+	{
+		searchBox.sendKeys(movieName);
+		sleep(10000);	
+	}
+	
+	public void  selectMovie(){
+		pushpaMovielistItem.click();
+	}
+	
+	public void captureMovieDetails()
+	{
+		
+		List<WebElement> items = movieDetailsList;
+		for(WebElement e: items)
+		{
+		    System.out.print(e.getText());
+		    if(e.getText().startsWith("Country of origin"))
+		    {
+		        System.out.println("ttttt"+e.getText());
+		    }
+		    else
+		    {
+		        System.out.println("  ");
+		    }
+		}
+	}
+	
+	
+	public void navigateToWiky()
+	{
+		driver.get("https://en.wikipedia.org/wiki/Main_Page");
+		sleep(10000);	
+	}
+	
+	public void searchMovieWiki(String movieName)
+	{
+		searchBoxWiki.sendKeys(movieName);
+		searchBoxWiki.sendKeys(Keys.ENTER);
+		sleep(10000);	
+	}
+	
+
+	public void captureWikiMovieDetails()
+	{
+		
+		List<WebElement> items = movieWikiDetailsList;
+		for(WebElement e: items)
+		{
+		    System.out.print(e.getText());
+		    if(e.getText().startsWith("Country"))
+		    {
+		        System.out.println("ttttt"+e.getText());
+		    }
+		    else
+		    {
+		        System.out.println("  ");
+		    }
+		}
+	}
+	
+	
+	
+	
+	public void sleep(int mSec)
+	{	
+		try {
+			Thread.sleep(mSec);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+	}
+}
---
+++ src/test/java/com/mov/qa/testcases/LoginPageTest.java
@@ -0,0 +1,61 @@
+package com.mov.qa.testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.mov.qa.base.TestBase;
+import com.mov.qa.pages.HomePage;
+import com.mov.qa.pages.LoginPage;
+
+public class LoginPageTest extends TestBase{
+	LoginPage loginPage;
+	HomePage homePage;
+	
+	public LoginPageTest(){
+		super();
+		 
+	}
+	
+	@BeforeMethod
+	public void setUp(){
+		initialization();
+		loginPage = new LoginPage();	
+	}
+	
+	@Test(priority=1)
+	public void loginPageTitleTest(){
+		String title = loginPage.validateLoginPageTitle();
+		System.out.println("====="+title+"=====");
+		Assert.assertEquals(title.trim(), "Sign in with IMDb - IMDb");
+		 loginPage.navigateToSigneIn();
+		Assert.assertTrue(true);
+		loginPage.sleep(1000);
+		homePage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+		loginPage.sleep(1000);
+		loginPage.searchMovie("Pushpa");
+		loginPage.sleep(10000);
+		loginPage.selectMovie();
+		loginPage.sleep(10000);
+		loginPage.captureMovieDetails();
+		loginPage.navigateToWiky();
+		loginPage.searchMovieWiki("Pushpa");
+		loginPage.captureWikiMovieDetails();			
+		
+		Assert.assertEquals(true, true);
+		
+	}
+
+	
+	
+	@AfterMethod
+	public void tearDown(){
+		driver.quit();
+	}
+	
+	
+	
+	
+
+}
